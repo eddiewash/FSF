83f14f59d2d56b13e9859324756eaf53a040f21c
MaddyMaxey <mmaxey0@gmail.com>
2013-05-12 14:53:09 -0400

    Adjusting for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ FSF::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += ['static_pages.css']
+  RAILS_ENV=production bundle exec rake assets:precompile
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
