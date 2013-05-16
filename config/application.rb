7211f2ae185d304b192c7e2318d3676598fde4a1
MaddyMaxey <mmaxey0@gmail.com>
2013-05-15 21:21:48 -0400

    changing config.assets

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module FSF
     config.assets.version = '1.0'
 
      # Required for Devise on Heroku
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
