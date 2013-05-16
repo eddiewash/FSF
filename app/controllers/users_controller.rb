3aaca2ef0f6b4a8816d152c4b95d6c6d0b7916a2
MaddyMaxey <mmaxey0@gmail.com>
2013-05-15 21:09:28 -0400

    pushing to Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,36 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
+	
+def new
+  @user = User.new
+  @title = "Sign up"
+end
+
+def show
+  if params[:id].nil? && current_user
+    @user = current_user
+  else
+    @user = User.find(params[:id])
   end
 
-  def show
-    @user = UserDecorator.find(params[:id])
+  @title = @user.name
   end
+
+
+def create
+  @user = User.new(params[:user])
+  if @user.save
+    sign_in @user
+    flash[:success] = "Account created!"
+    redirect_to @user
+   else
+     @title = "Sign up"
+     render 'new'
+   end
+  end
+
+def destroy
+  @user.destroy
+  redirect_to users_path, :flash => { :success => "User destroyed." }
+ end
+
 end
